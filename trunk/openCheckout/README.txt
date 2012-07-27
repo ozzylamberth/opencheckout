449b25e59fdd191197471856e3996e5b238e809a
jorgejarainostroza@gmail.com <jorgejarainostroza@gmail.com@845f88a6-cc0f-4cc2-90f0-15df4f04cb32>
2012-07-27 20:05:22 +0000

    Importo la primera (y por el momento, inútil) versión de openCheckout.

---
+++ trunk/openCheckout/README.txt
@@ -0,0 +1,7 @@
+openCheckout 0.1
+================
+
+
+ES: Es un sencillo software de Punto de Venta. EN: It's a simple Point of Sale System (PoS).
+
+ES: Hecho en Java. EN: Made in Java.
---
+++ trunk/openCheckout/src/jorgejarai/javase/opencheckout/Main.java
@@ -0,0 +1,34 @@
+/*
+ * openCheckout 0.1
+ *
+ * @version 0.1
+ * @author Jorge Jara <jorgejarainostroza@gmail.com>
+ *
+ * Esta clase sirve para arrancar el sistema de 'log-eo' y a la vez inicializar
+ * el programa.
+ *
+ * Copyright (C) 2012 Jorge Jara Inostroza.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with ths program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package jorgejarai.javase.opencheckout;
+
+public class Main {
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
